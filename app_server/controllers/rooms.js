c22a95a805795512ad76d3a6c85a4d19cb9dff0f
Mike Ross <mrosstech@gmail.com>
2024-01-28 13:46:06 -0500

    completed module3

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+
+var roomTypes = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
 /* Get rooms page */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Rooms Page', roomsSelected: true });
+    res.render('rooms', { title: 'Rooms Page', roomsSelected: true, roomTypes });
 };
 
 module.exports = {
